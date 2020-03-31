ce1cf26ca4f8ad41b26908fc4c2ced4caac0feb2
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-03-31 10:40:55 -0700

    :bento: add interfaces

---
+++ src/interfaces/EducationData.interface.ts
@@ -0,0 +1,15 @@
+/**
+ * Education
+ */
+export interface EducationData {
+    /** Educational institute name */
+    name: string;
+    /** Date */
+    date?: string;
+    /** Study duration */
+    duration?: string;
+    /** Output */
+    output?: string;
+}
+
+export default EducationData;

f0e11b00c9d80744b660c6c96b42d4c51cc2e99c
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-03-31 10:43:37 -0700

    :bento: add constants

---
+++ src/const/technologies.const.ts
@@ -0,0 +1,24 @@
+export const technologies = [
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "Angular 2+",
+    "HTML5",
+    "CSS3",
+    "Git",
+    "Emotion",
+    "UX",
+    "Angular Material UI",
+    "Jest",
+    "SASS",
+    "BEM",
+    "Jira",
+    "Confluence",
+    "Bootstrap 3",
+    "AngularJS",
+    "HLS",
+    "Scrum",
+    "Kanban"
+];
+
+export default technologies;

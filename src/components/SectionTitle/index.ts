9bd63d8a965709927774232298e010e5426caa19
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-03-31 10:53:27 -0700

    :bento: add SideSection component

---
+++ src/components/SectionTitle/index.ts
@@ -0,0 +1,1 @@
+export { default, SectionTitle } from "./SectionTitle";
---
+++ src/components/SideSection/SideSectionProps.ts
@@ -0,0 +1,9 @@
+/**
+ * Props
+ */
+export interface SideSectionProps {
+    /** Skills data */
+    technologies: string[];
+}
+
+export default SideSectionProps;

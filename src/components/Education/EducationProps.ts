30ef627d6f3c75dd978268a3920e17ca83556395
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-03-31 10:54:06 -0700

    :bento: add Education component

---
+++ src/components/Education/EducationProps.ts
@@ -0,0 +1,11 @@
+/**
+ * Props
+ */
+export interface EducationProps {
+    /** Education name */
+    educationName: string;
+    /** Date */
+    date: string;
+}
+
+export default EducationProps;

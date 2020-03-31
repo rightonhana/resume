00e3a8cc6df765a85340a48f3da7b61420b5ba19
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-03-31 10:47:16 -0700

    :bento: add Language component

---
+++ src/components/Language/LanguageProps.ts
@@ -0,0 +1,11 @@
+/**
+ * Props
+ */
+export interface LanguageProps {
+    /** Language name */
+    language: string;
+    /** Level */
+    level: string;
+}
+
+export default LanguageProps;
---
+++ src/components/Language/index.ts
@@ -0,0 +1,1 @@
+export { default, Language } from "./Language";

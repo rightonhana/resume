19470c1f56dfbfe04a785995de8f32be3c36e8d1
Florencia Gonzalez <rightonhana@gmail.com>
2021-03-29 13:29:20 -0700

    :bug: fix png import

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -2,12 +2,13 @@ import React, { FC } from "react";
 import { SectionTitle } from "../SectionTitle/sectionTitle";
 import { introductionProps } from "./IntroductionProps";
 import styles from "./Introduction.module.css";
+import photo from "/profile-photo.png";
 
 export const Introduction: FC<introductionProps> = ({intro, ...props}) => (
 	<section {...props}>
 		<SectionTitle title="About me"/>
 		<header className={styles.Header}>
-			<img className={styles.Image} src="/profile-photo.png" alt="Profile photo"/>
+			<img className={styles.Image} src={photo} alt="Profile photo"/>
 			<h1>Florencia Gonzalez</h1>
 		</header>
 		<p>{intro}</p>

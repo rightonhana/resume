bfedd3438da996247fd83e5fdc74fe7f8d669b24
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-08 11:48:05 -0700

    :lipstick: update PersonalInfo styles

--- src/components/PersonalInfo/PersonalInfo.tsx
+++ src/components/PersonalInfo/PersonalInfo.tsx
@@ -3,12 +3,14 @@ import { SectionTitle } from "../SectionTitle/sectionTitle";
 import styles from "./PersonalInfo.module.css";
 
 export const PersonalInfo: FC<{}> = ({...props}) => (
-	<section className={styles.Info}>
+	<section {...props}>
 		<SectionTitle title="Personal Info"/>
-		<span><strong>Date of Birth</strong>: 09/30/1995</span>
-		<span><strong>From</strong>: Mar del Plata, Buenos Aires, Argentina</span>
-		<span><strong>Living in</strong>: Bellevue, Washington, USA</span>
-		<span><strong>Marital status</strong>: Married</span>
-		<span><strong>Contact</strong>: <a href="https://rightonhana.me" rel="noopener noreferrer">rightonhana.me</a></span>
+		<div className={styles.Data}>
+			<span><strong>Date of Birth</strong>: 09/30/1995</span>
+			<span><strong>From</strong>: Mar del Plata, Buenos Aires, Argentina</span>
+			<span><strong>Living in</strong>: Bellevue, Washington, USA</span>
+			<span><strong>Marital status</strong>: Married</span>
+			<span><strong>Contact</strong>: <a href="https://rightonhana.me" rel="noopener noreferrer">rightonhana.me</a></span>
+		</div>
 	</section>
 );

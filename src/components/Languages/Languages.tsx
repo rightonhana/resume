a2974d147854f1d852acdae3299376ea20dec9fa
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-08 11:48:55 -0700

    :lipstick: update Languages styles

--- src/components/Languages/Languages.tsx
+++ src/components/Languages/Languages.tsx
@@ -1,17 +1,19 @@
 import React, { FC } from "react";
 import { SectionTitle } from "../SectionTitle/sectionTitle";
-import style from "./Languages.module.css";
+import styles from "./Languages.module.css";
 
 export const Languages: FC<{}> = ({
 	...props
 }) => (
-	<section className={style.Languages} {...props}>
+	<section {...props}>
 		<SectionTitle title="Languages" />
-		<span>
-			<strong>English</strong>: Upper Intermediate
-		</span>
-		<span>
-			<strong>Spanish</strong>: Native
-		</span>
+		<div className={styles.LanguagesList}>
+			<span>
+				<strong>English</strong>: Upper Intermediate
+			</span>
+			<span>
+				<strong>Spanish</strong>: Native
+			</span>
+		</div>
 	</section>
 );

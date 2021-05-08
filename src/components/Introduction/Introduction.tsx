f41c1dbe33ee5e57882fa3558d87b3e052ac3b76
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-08 11:56:05 -0700

    :lipstick: update Introduction

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -7,10 +7,12 @@ import photo from "/profile-photo.png";
 export const Introduction: FC<introductionProps> = ({intro, ...props}) => (
 	<section {...props}>
 		<SectionTitle title="About me"/>
-		<header className={styles.Header}>
-			<img className={styles.Image} src={photo} alt="Profile photo"/>
-			<h1>Florencia Gonzalez</h1>
-		</header>
-		<p>{intro}</p>
+		<div className={styles.AboutMe}>
+			<header className={styles.Header}>
+				<img className={styles.Image} src={photo} alt="Profile photo"/>
+				<h1>Florencia Gonzalez</h1>
+			</header>
+			<p className={styles.Intro}>{intro}</p>
+		</div>
 	</section>
 );
